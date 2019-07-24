a5748507d37ea8d23e3438e19f82854db57ae68b
Kent Reese <kentreese64@gmail.com>
2019-07-24 13:45:07 -0400

    J'amis tu.

---
+++ OverDriveAuthoring/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace OverDriveAuthoring
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Item";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Lane";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(496, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Load File";
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(499, 25);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(33, 20);
+            this.fileButton.TabIndex = 0;
+            this.fileButton.Text = "...";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(637, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Save File";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(640, 25);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(33, 20);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "...";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fileButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "OverDrive Authoring";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ OverDriveAuthoring/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OverDriveAuthoring
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        /// <summary>
+        /// Loads the Label track from a file given it has appropriate data.
+        /// </summary>
+        /// <param name="file"></param>
+        private void fileLoad(string file)
+        {
+
+            string s = File.ReadAllText(file);
+
+            int yPosition = 24;
+            int rowNumber = 1;
+            foreach( string line in s.Split('\n') )
+            {
+                
+                string[] values = line.Split(' ');
+                if( values.Length < 3 )
+                {
+                    continue;
+                }
+                ComboBox itemBox = new ComboBox();
+                ComboBox placeBox = new ComboBox();
+                TextBox timeBox = new TextBox();
+                //Time Box
+                timeBox.Location = new System.Drawing.Point(12, yPosition);
+                timeBox.Name = "timeBox" + rowNumber;
+                timeBox.Size = new System.Drawing.Size(212, 20);
+                timeBox.TabIndex = (rowNumber * 3) - 3;
+                timeBox.Text = values[0];
+
+                //Item Box
+                itemBox.FormattingEnabled = true;
+                itemBox.Items.AddRange(new object[] {
+                 "Note",
+                 "Cone",
+                 "Pothole",
+                 "Oil",
+                 "Spec",
+                 "Car1",
+                 "Car2",
+                 "Car3",
+                 "Car4",
+                 "Car5",
+                 "Health",
+                 "Coin",
+                 "Win",
+                 "TutNote",
+                 "TutOil",
+                 "TutObs",
+                 "TutSpec"});
+                itemBox.Location = new System.Drawing.Point(230, yPosition);
+                itemBox.Name = "itemBox" + rowNumber;
+                itemBox.Text = values[1];
+                itemBox.Size = new System.Drawing.Size(82, 21);
+                itemBox.TabIndex = (rowNumber * 3) - 2;
+
+                //Place Box
+                placeBox.FormattingEnabled = true;
+                placeBox.Items.AddRange(new object[] {
+                "Mid",
+                "Left",
+                "Right"});
+                placeBox.Text = values[2];
+                placeBox.Location = new System.Drawing.Point(318, yPosition);
+                placeBox.Name = "placeBox" + rowNumber;
+                placeBox.Size = new System.Drawing.Size(82, 21);
+                placeBox.TabIndex = (rowNumber * 3) - 1;
+
+                Controls.Add(timeBox);
+                Controls.Add(itemBox);
+                Controls.Add(placeBox);
+
+                yPosition += 26;
+                rowNumber++;
+                if(values[1] == "Win")
+                {
+                    break;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Saves all relevant fields to the text file of your choosing 
+        /// </summary>
+        /// <param name="file"></param>
+        private void fileSave(string file)
+        {
+            //Nothing right now
+            int rowNumber = 1;
+            ComboBox itemBox = Controls.Find("itemBox" + rowNumber, true).FirstOrDefault() as ComboBox;
+            ComboBox placeBox = Controls.Find("placeBox" + rowNumber, true).FirstOrDefault() as ComboBox;
+             TextBox timeBox = Controls.Find("timeBox" + rowNumber, true).FirstOrDefault() as TextBox;
+
+            string text = "";
+            while(placeBox != null)
+            {
+                text = text + timeBox.Text + " " + itemBox.Text + " " + placeBox.Text + "\n";
+                rowNumber++;
+                itemBox = Controls.Find("itemBox" + rowNumber, true).FirstOrDefault() as ComboBox;
+                placeBox = Controls.Find("placeBox" + rowNumber, true).FirstOrDefault() as ComboBox;
+                timeBox = Controls.Find("timeBox" + rowNumber, true).FirstOrDefault() as TextBox;
+            }
+
+            File.WriteAllText(file, text);
+        }
+
+        private void fileButton_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    fileLoad(file);
+                }
+                catch (IOException)
+                {
+                }
+            }
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    // Save file
+                    fileSave(file);
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+    }
+}
